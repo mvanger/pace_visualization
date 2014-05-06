60c4d0604468867cfe403c36ff85782fa33cc188
mvanger <mvanger@nyu.edu>
2014-05-06 13:37:32 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PaceViz::Application.config.secret_key_base = 'b75707cc7c76a226cd92bcd30f60be53bea6ec32e48ef1db9977f013ad2d01b22e9689b9856971bd471f4eb6dcd8c5d00578df75a633878fd1935fdbf3cebb38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PaceViz::Application.config.session_store :cookie_store, key: '_pace_viz_session'
