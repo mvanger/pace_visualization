929f570591483977549ecabbb07cfb9cccc096e9
mvanger <mvanger@nyu.edu>
2014-08-12 18:54:36 -0400

    removed user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 PaceViz::Application.routes.draw do
-  # root to: 'charts#index'
+  root to: 'charts#chord_two'
 
-  root to: 'sessions#new'
-  resources :sessions, only: [:new, :create, :destroy]
-  get 'chord_two' => 'charts#chord_two', as: "chords"
+  # root to: 'sessions#new'
+  # resources :sessions, only: [:new, :create, :destroy]
+  # get 'chord_two' => 'charts#chord_two', as: "chords"
 end
